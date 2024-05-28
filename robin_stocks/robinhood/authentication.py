9e204491cf1efeae56dffbbb5b4ca8f15a1442db
Rekesh Ali <rekeshali@gmail.com>
2024-05-28 03:40:48 -0400

    async auth

--- robin_stocks/robinhood/authentication.py
+++ robin_stocks/robinhood/authentication.py
@@ -3,6 +3,7 @@ import getpass
 import os
 import pickle
 import random
+import aiofiles
 
 from robin_stocks.robinhood.helper import *
 from robin_stocks.robinhood.urls import *
@@ -33,7 +34,7 @@ def generate_device_token():
     return(id)
 
 
-def respond_to_challenge(challenge_id, sms_code):
+async def respond_to_challenge(challenge_id, sms_code):
     """This function will post to the challenge url.
 
     :param challenge_id: The challenge id.
@@ -47,10 +48,10 @@ def respond_to_challenge(challenge_id, sms_code):
     payload = {
         'response': sms_code
     }
-    return(request_post(url, payload))
+    return(await request_post(url, payload))
 
 
-def login(username=None, password=None, expiresIn=86400, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name=""):
+async def login(username=None, password=None, expiresIn=86400, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name=""):
     """This function will effectively log the user into robinhood by getting an
     authentication token and saving it to the session header. By default, it
     will store the authentication token in a pickle file and load that value
@@ -82,8 +83,9 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
     device_token = generate_device_token()
     home_dir = os.path.expanduser("~")
     data_dir = os.path.join(home_dir, ".tokens")
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+    # Make directory if it does not exist
+    if not await aiofiles.os.path.exists(data_dir):
+        await aiofiles.os.mkdir(data_dir)
     creds_file = "robinhood" + pickle_name + ".pickle"
     pickle_path = os.path.join(data_dir, creds_file)
     # Challenge type is used if not logging in with two-factor authentication.
@@ -108,13 +110,13 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
         payload['mfa_code'] = mfa_code
 
     # If authentication has been stored in pickle file then load it. Stops login server from being pinged so much.
-    if os.path.isfile(pickle_path):
+    if await aiofiles.os.path.exists(pickle_path):
         # If store_session has been set to false then delete the pickle file, otherwise try to load it.
-        # Loading pickle file will fail if the acess_token has expired.
+        # Loading pickle file will fail if the access_token has expired.
         if store_session:
             try:
-                with open(pickle_path, 'rb') as f:
-                    pickle_data = pickle.load(f)
+                async with aiofiles.open(pickle_path, 'rb') as f:
+                    pickle_data = pickle.load(await f.read())
                     access_token = pickle_data['access_token']
                     token_type = pickle_data['token_type']
                     refresh_token = pickle_data['refresh_token']
@@ -126,9 +128,9 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
                     update_session(
                         'Authorization', '{0} {1}'.format(token_type, access_token))
                     # Try to load account profile to check that authorization token is still valid.
-                    res = request_get(
+                    res = await request_get(
                         positions_url(), 'pagination', {'nonzero': 'true'}, jsonify_data=False)
-                    # Raises exception is response code is not 200.
+                    # Raises exception if response code is not 200.
                     res.raise_for_status()
                     return({'access_token': access_token, 'token_type': token_type,
                             'expires_in': expiresIn, 'scope': scope, 'detail': 'logged in using authentication in {0}'.format(creds_file),
@@ -139,7 +141,7 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
                 set_login_state(False)
                 update_session('Authorization', None)
         else:
-            os.remove(pickle_path)
+            await aiofiles.os.remove(pickle_path)
 
     # Try to log in normally.
     if not username:
@@ -150,30 +152,30 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
         password = getpass.getpass("Robinhood password: ")
         payload['password'] = password
 
-    data = request_post(url, payload)
+    data = await request_post(url, payload)
     # Handle case where mfa or challenge is required.
     if data:
         if 'mfa_required' in data:
             mfa_token = input("Please type in the MFA code: ")
             payload['mfa_code'] = mfa_token
-            res = request_post(url, payload, jsonify_data=False)
-            while (res.status_code != 200):
+            res = await request_post(url, payload, jsonify_data=False)
+            while res.status != 200:
                 mfa_token = input(
                     "That MFA code was not correct. Please type in another MFA code: ")
                 payload['mfa_code'] = mfa_token
-                res = request_post(url, payload, jsonify_data=False)
-            data = res.json()
+                res = await request_post(url, payload, jsonify_data=False)
+            data = await res.json()
         elif 'challenge' in data:
             challenge_id = data['challenge']['id']
             sms_code = input('Enter Robinhood code for validation: ')
-            res = respond_to_challenge(challenge_id, sms_code)
+            res = await respond_to_challenge(challenge_id, sms_code)
             while 'challenge' in res and res['challenge']['remaining_attempts'] > 0:
                 sms_code = input('That code was not correct. {0} tries remaining. Please type in another code: '.format(
                     res['challenge']['remaining_attempts']))
-                res = respond_to_challenge(challenge_id, sms_code)
+                res = await respond_to_challenge(challenge_id, sms_code)
             update_session(
                 'X-ROBINHOOD-CHALLENGE-RESPONSE-ID', challenge_id)
-            data = request_post(url, payload)
+            data = await request_post(url, payload)
         # Update Session data with authorization or raise exception with the information present in data.
         if 'access_token' in data:
             token = '{0} {1}'.format(data['token_type'], data['access_token'])
@@ -181,16 +183,16 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
             set_login_state(True)
             data['detail'] = "logged in with brand new authentication code."
             if store_session:
-                with open(pickle_path, 'wb') as f:
-                    pickle.dump({'token_type': data['token_type'],
-                                 'access_token': data['access_token'],
-                                 'refresh_token': data['refresh_token'],
-                                 'device_token': payload['device_token']}, f)
+                async with aiofiles.open(pickle_path, 'wb') as f:
+                    await f.write(pickle.dumps({'token_type': data['token_type'],
+                                                'access_token': data['access_token'],
+                                                'refresh_token': data['refresh_token'],
+                                                'device_token': payload['device_token']}))
         else:
             raise Exception(data['detail'])
     else:
         raise Exception('Error: Trouble connecting to robinhood API. Check internet connection.')
-    return(data)
+    return data
 
 
 @login_required
